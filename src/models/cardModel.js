36c1346220797c5ff4c9f284c9c0a415db5d815e
doananhkiet886 <doananhkiet886@gmail.com>
2024-01-19 00:50:46 +0700

    add create card api

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -4,7 +4,7 @@ import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 const COLLECTION_NAME = 'cards'
 const COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().min(3).max(50).trim().strict().required(),
-  cover: Joi.string().min(7).trim().strict().required(),
+  cover: Joi.string().min(7).trim().strict().default(null),
   description: Joi.string().min(3).max(256).trim().strict().default(null),
   slug: Joi.string().min(3).trim().strict().required(),
   boardId: Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE).required(),
