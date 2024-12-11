fd98d3de0dbf88ceab79d6cd8d2e36ed547b41c4
doankietdev <doankietdev@gmail.com>
2024-12-11 11:52:04 +0700

    refactor: load whitelist domains from env

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import { DEV_ENV } from '~/utils/constants'
 
-const { NODE_ENV, MONGODB_URI, DATABASE_NAME, APP_HOST, APP_PORT } = process.env
+const { NODE_ENV, MONGODB_URI, DATABASE_NAME, APP_PORT, PORT, WHITE_LIST_DOMAINS } = process.env
 
 export const BUILD_MODE = NODE_ENV || DEV_ENV
 export const APP = {
-  HOST: APP_HOST || 'localhost',
-  PORT: APP_PORT || 5600
+  PORT: APP_PORT || PORT || 5600,
+  WHITE_LIST_DOMAINS: WHITE_LIST_DOMAINS.split(',') || []
 }
 export const MONGODB = {
-  URI: MONGODB_URI || '',
-  DATABASE_NAME: DATABASE_NAME || ''
+  URI: MONGODB_URI || 'mongodb://127.0.0.1:27017',
+  DATABASE_NAME: DATABASE_NAME || 'trello'
 }
