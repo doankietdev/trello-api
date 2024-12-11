4dc41ba6d8fe9226ffa05bbb394d754d3a0aef48
doankietdev <doankietdev@gmail.com>
2024-12-11 12:17:07 +0700

    fix bugs

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,12 +1,19 @@
 import 'dotenv/config'
 import { DEV_ENV } from '~/utils/constants'
 
-const { NODE_ENV, MONGODB_URI, DATABASE_NAME, APP_PORT, PORT, WHITE_LIST_DOMAINS } = process.env
+const {
+  NODE_ENV,
+  MONGODB_URI,
+  DATABASE_NAME,
+  APP_PORT,
+  PORT,
+  WHITE_LIST_DOMAINS = []
+} = process.env
 
 export const BUILD_MODE = NODE_ENV || DEV_ENV
 export const APP = {
   PORT: APP_PORT || PORT || 5600,
-  WHITE_LIST_DOMAINS: WHITE_LIST_DOMAINS.split(',') || []
+  WHITE_LIST_DOMAINS: WHITE_LIST_DOMAINS.split(',')
 }
 export const MONGODB = {
   URI: MONGODB_URI || 'mongodb://127.0.0.1:27017',
